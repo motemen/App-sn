dbb669a9b22b8224712251870b66515f043648ad
motemen <motemen@gmail.com>
2011-04-18 19:18:19 +0900

    default to help

--- sn.pl
+++ sn.pl
@@ -1,13 +1,12 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-no  warnings 'once';
 use lib 'lib';
 use App::sn::Command;
 
 $|++;
 binmode STDOUT, ':utf8';
 
-App::sn::Command->dispatch;
+@ARGV = ('help') unless @ARGV;
 
-# App::sn::Command->storage->save;
+App::sn::Command->dispatch;
