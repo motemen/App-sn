3aac85ce84d79ebe531b15ee1c3b6d3edb74a8e4
motemen <motemen@gmail.com>
2011-04-18 19:17:19 +0900

    keep local token

--- lib/App/sn/API.pm
+++ lib/App/sn/API.pm
@@ -19,16 +19,35 @@ sub new {
     my $growl = Growl::Any->new(appname => 'sn.pl', events => [ 'Note created', 'Note updated' ]);
 
     return bless {
-        api_root  => 'https://simple-note.appspot.com',
-        config    => $config,
-        ua        => $ua,
-        growl     => $growl,
+        api_root => 'https://simple-note.appspot.com',
+        config   => $config,
+        ua       => $ua,
+        growl    => $growl,
     }, $class;
 }
 
+sub app { 'App::sn::Command' }
+
 sub token {
     my $self = shift;
-    return $self->{token} ||= $self->_build_token;
+    
+    return $self->{token} if $self->{token};
+
+    if (my $auth = $self->app->local_data->{auth}) {
+        # expire local token in 12 hours
+        if ($auth->{time} + 12 * 60 * 60 < time()) {
+            $self->expire_local_auth_token;
+        } else {
+            return $auth->{token};
+        }
+    }
+
+    return $self->{token} = $self->_build_token;
+}
+
+sub expire_local_auth_token {
+    my $self = shift;
+    delete $self->app->local_data->{auth};
 }
 
 sub _build_token {
@@ -40,8 +59,14 @@ sub _build_token {
             sprintf 'email=%s&password=%s', uri_escape($self->{config}->{email}), uri_escape($self->{config}->{password})
         )
     );
+    warn "POST $self->{api_root}/api/login\n" if $ENV{DEBUG_SN_PL};
     die $res->status_line unless $res->is_success;
-    return $res->content;
+    my $token = $res->content;
+    $self->app->local_data->{auth} = {
+        time => time(),
+        token => $token,
+    };
+    return $token;
 }
 
 sub get {
@@ -52,8 +77,13 @@ sub get {
         auth  => $self->token,
         %$params
     );
-    warn "GET $url\n" if $ENV{DEBUG_SN_PL};
     my $res = $self->{ua}->get($url);
+    warn "GET $url\n" if $ENV{DEBUG_SN_PL};
+    if ($res->code eq '401' && !$self->{auth_retry}) {
+        $self->{auth_retry}++;
+        $self->expire_local_auth_token;
+        return &get;
+    }
     die $res->status_line if $res->is_error;
     return decode_json $res->content;
 }
@@ -65,6 +95,11 @@ sub post {
         Content => encode_json($data),
     );
     warn "POST $self->{api_root}/api2/$path\n" if $ENV{DEBUG_SN_PL};
+    if ($res->code eq '401' && $self->{auth_retry}) {
+        $self->{auth_retry}++;
+        $self->expire_local_auth_token;
+        return &post;
+    }
     die $res->status_line if $res->is_error;
     return decode_json $res->content;
 }
